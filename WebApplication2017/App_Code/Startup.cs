c45a377838558f53df5fd4f656ebe919ebf26f95
shijith Ss <shijith.ss@gmail.com>
2020-01-03 19:57:29 +0530

    Add project files.

---
+++ WebApplication2017/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication2017.Startup))]
+namespace WebApplication2017
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
